c9ab3131596a63f783f72c89c4d705b2781c89bd
onyekachila <onyekachila@gmail.com>
2019-08-28 08:29:13 +0100

    Added entities with their relationships

---
+++ VOD.Common/Entities/Instructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VOD.Common.Entities
+{
+    public class Instructor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(80), Required]
+        public string Name { get; set; }
+        [MaxLength(1024)]
+        public string Description { get; set; }
+        [MaxLength(1024)]
+        public string Thumbnail { get; set; }
+    }
+}
