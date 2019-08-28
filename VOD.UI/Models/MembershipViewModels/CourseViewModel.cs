46b26eb6dcf9573bdf6d527de3ee583baa131c12
onyekachila <onyekachila@gmail.com>
2019-08-28 08:19:33 +0100

    Added DTOs for model classes and viewmodels the web ui projects

---
+++ VOD.UI/Models/MembershipViewModels/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VOD.Common.DTOModels.UI;
+
+namespace VOD.UI.Models.MembershipViewModels
+{
+    public class CourseViewModel
+    {
+        public CourseDTO Course { get; set; }
+        public InstructorDTO Instructor { get; set; }
+        public IEnumerable<ModuleDTO> Modules { get; set; }
+    }
+}
---
+++ VOD.UI/Models/MembershipViewModels/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VOD.Common.DTOModels.UI;
+
+namespace VOD.UI.Models.MembershipViewModels
+{
+    public class DashboardViewModel
+    {
+        public List<List<CourseDTO>> Courses { get; set; }
+    }
+}
