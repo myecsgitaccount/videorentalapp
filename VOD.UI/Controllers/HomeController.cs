dc59ddf813b6cc286b7c72ebb3a33db66e71f11a
onyekachila <onyekachila@gmail.com>
2019-08-28 07:30:28 +0100

    Added login page and css using material design

--- VOD.UI/Controllers/HomeController.cs
+++ VOD.UI/Controllers/HomeController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using VOD.Common.Entities;
 using VOD.UI.Models;
 
 namespace VOD.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private SignInManager<VODUser> _signInManager;
+
+        public HomeController(SignInManager<VODUser> signInMgr)
+        {
+            _signInManager = signInMgr;
+        }
+
         public IActionResult Index()
         {
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToPage("/Account/Login",
+                new { Area = "Identity" });
             return View();
         }
 
