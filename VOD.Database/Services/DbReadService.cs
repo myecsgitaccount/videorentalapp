d2c5be12b718471a5120435a6f0ac567805d76c5
onyekachila <onyekachila@gmail.com>
2019-08-31 02:14:16 +0100

    added read service to access data from the database

---
+++ VOD.Database/Services/DbReadService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using VOD.Database.Context;
+
+namespace VOD.Database.Services
+{
+    public class DbReadService : IDbReadService
+    {
+        private VODContext _db;
+
+        public DbReadService(VODContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> AnyAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class
+        {
+            return await _db.Set<TEntity>().Where(expression).AnyAsync(); 
+        }
+
+        public async Task<List<TEntity>> GetAsync<TEntity>() where TEntity : class
+        {
+            return await _db.Set<TEntity>().ToListAsync(); 
+        }
+
+        public async Task<List<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class
+        {
+            return await _db.Set<TEntity>().Where(expression).ToListAsync(); 
+        }
+
+        public async Task<TEntity> SingleAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class
+        {
+            return await _db.Set<TEntity>().Where(expression).SingleOrDefaultAsync(); 
+        }
+
+        public void Include<TEntity>() where TEntity : class
+        {
+            var propertyNames = _db.Model
+                .FindEntityType(typeof(TEntity))
+                .GetNavigations()
+                .Select(e => e.Name);
+
+            foreach (var name in propertyNames)
+                _db.Set<TEntity>().Include(name).Load();
+        }
+
+        public void Include<TEntity1, TEntity2>() where TEntity1 : class where TEntity2 : class
+        {
+            Include<TEntity1>();
+            Include<TEntity2>();
+        }
+    }
+}
